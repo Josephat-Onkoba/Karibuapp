36bba402002ec2bbd84fe41a467baa276b422874
Josephat-Onkoba <josephatonkoba30@gmail.com>
2025-05-28 13:27:13 +0300

    solve  a bug

--- app/Http/Controllers/Usher/RegisterController.php
+++ app/Http/Controllers/Usher/RegisterController.php
@@ -842,7 +842,7 @@ class RegisterController extends Controller
                 $message .= "Please complete payment to attend.";
             } elseif ($participant->payment_status === 'Paid via M-Pesa' || $participant->payment_status === 'Paid via Vabu') {
                 $message .= "Welcome to Zetech University"; 
-                $message .= "For more information about the conference, visit https://conference.zetech.ac.ke You can also view the full program at https://conference.zetech.ac.ke/index.php/conference-2025/program" }
+                $message .= "For more information about the conference, visit https://conference.zetech.ac.ke You can also view the full program at https://conference.zetech.ac.ke/index.php/conference-2025/program"}
             
             // Send SMS with retry logic
             $retryCount = 0;
